3c473ae0ba9a018d9a31a805894baa191891b800
zandalman <zandalman@gmail.com>
2024-10-06 17:49:11 -0400

    cleanup: renamed unused loop index

--- src/MCHammer/hammer.py
+++ src/MCHammer/hammer.py
@@ -1,12 +1,22 @@
 from __future__ import annotations
+
 import numpy as np
 
 
 class Hammer:
     def __init__(
-      self, outfile_name, num_step, num_walk, num_param, log_prob_func, log_prob_args, std_proposal, initial, frac_burn=0.2
+        self,
+        outfile_name,
+        num_step,
+        num_walk,
+        num_param,
+        log_prob_func,
+        log_prob_args,
+        std_proposal,
+        initial,
+        frac_burn=0.2,
     ):
-        '''
+        """
         Initialize the Hammer object.
 
         Args
@@ -23,8 +33,8 @@ class Hammer:
         Attrs
             samples (np.ndarray): The array of samples.
             step_current (int):   The current step.
-            rate_accept (float):  The acceptance rate. 
-        '''
+            rate_accept (float):  The acceptance rate.
+        """
         self.outfile_name = outfile_name
         self.num_step = num_step
         self.num_walk = num_walk
@@ -34,7 +44,7 @@ class Hammer:
         self.std_proposal = std_proposal
         self.frac_burn = frac_burn
         self.initial = initial
-        
+
         self.rng = np.random.default_rng()
         self.samples = np.zeros((self.num_step, self.num_walk, self.num_param))
         self.samples[0] = self.initial
@@ -42,31 +52,33 @@ class Hammer:
         self.rate_accept = 0.0
 
     def step(self):
-        '''
+        """
         Step the Metropolis-Hastings algorithm.
-        '''
+        """
         current = self.samples[self.step_current]
-        proposal = current + self.rng.normal(0, self.std_proposal, size=(self.num_walk, self.num_param))
-        
+        proposal = current + self.rng.normal(
+            0, self.std_proposal, size=(self.num_walk, self.num_param)
+        )
+
         log_prob_current = self.log_prob_func(current, *self.log_prob_args)
         log_prob_proposal = self.log_prob_func(proposal, *self.log_prob_args)
         prob_accept = np.exp(log_prob_proposal - log_prob_current)
 
-        cond_accept = self.rng.random((self.num_walk)) < prob_accept
+        cond_accept = self.rng.random(self.num_walk) < prob_accept
         if self.step_current > self.frac_burn * self.num_step:
             self.rate_accept += np.sum(cond_accept)
-    
+
         self.samples[self.step_current + 1] = current
         self.samples[self.step_current + 1, cond_accept] = proposal[cond_accept]
 
         self.step_current += 1
 
     def run(self):
-        '''
+        """
         Run the Metropolis-Hastings algorithm.
-        '''
-        for i in range(self.num_step - 1):
+        """
+        for _i in range(self.num_step - 1):
             self.step()
-        
-        self.samples = self.samples[int(self.frac_burn * self.num_step):]
-        self.rate_accept /= (self.num_step * self.num_walk * (1 - self.frac_burn))
+
+        self.samples = self.samples[int(self.frac_burn * self.num_step) :]
+        self.rate_accept /= self.num_step * self.num_walk * (1 - self.frac_burn)
