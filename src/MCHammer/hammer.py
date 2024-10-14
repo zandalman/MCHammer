266f339938e0c7d37fbc7a7136d68512aa6aba9e
zandalman <zandalman@gmail.com>
2024-10-14 08:32:35 -0500

    feat: completely updated hammer class

--- src/MCHammer/hammer.py
+++ src/MCHammer/hammer.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
-
 import numpy as np
+import abc
 
 __all__ = ["Hammer"]
 
@@ -9,75 +9,95 @@ def __dir__():
     return __all__
 
 
-class Hammer:
+class Hammer(abc.ABC):
+    """
+    The Hammer object.
+
+    Args
+        state_init (np.ndarray): The initial state.
+        std_prop (float):        The standard deviation of the proposal distirbution.
+        num_step (int):          The number of steps.
+        num_walk (int):          The number of walkers.
+        num_dim (int):           The number of dimensions.
+        path_outfile (str):      The path to the output HDF5 file.
+        frac_burn (float):       The burn fraction.
+        seed (int):              The random number generator seed.
+        log_prob_args (dict):    The arguments for the log probability function.
+
+    Attr
+        idx_step (int):                       The index of the current step.
+        rate_accept(float):                   The acceptance rate.
+        state_curr (np.ndarray):              The current state.
+        rng (np.random._generator.Generator): The random number generator.
+        num_sample (int):                     The number of samples.
+        samples (np.ndarray):                 The sample array.
+    """
     def __init__(
-        self,
-        outfile_name,
-        num_step,
-        num_walk,
-        num_param,
-        log_prob_func,
-        log_prob_args,
-        std_proposal,
-        initial,
-        frac_burn=0.2,
-    ):
-        """
-        Initialize the Hammer object.
-
-        Args
-            outfile_name (string):    The name of the output HDF5 file.
-            num_step (int):           The number of steps.
-            num_walk (int):           The number of walkers.
-            num_param (int):          The number of parameters.
-            log_prob_func (function): The log probability function of the target distribution.
-            log_prob_args (tuple):    The arguments for the log probability function of the target distribution.
-            std_proposal (float):     The STD of the proposal distribution.
-            initial (np.ndarray):     The array of initial walker positions.
-            frac_burn (float):        The burn fraction.
-
-        Attrs
-            samples (np.ndarray): The array of samples.
-            step_current (int):   The current step.
-            rate_accept (float):  The acceptance rate.
-        """
-        self.outfile_name = outfile_name
+            self, 
+            state_init, 
+            std_prop, 
+            num_step, 
+            num_walk, 
+            num_dim, 
+            path_outfile=None, 
+            frac_burn=0.2, 
+            seed=None, 
+            *args,
+            **kwargs
+        ):
+        
+        assert (frac_burn >= 0) and (frac_burn <= 1), \
+            "Burn fraction %.3g outside the allowed range [0, 1]." % frac_burn
+        
+        assert state_init.shape == (num_walk, num_dim), \
+            "Initial state shape (%d, %d) does not match number of walkers and number of dimensions (%d, %d)" % \
+            (state_init.shape[0], state_init.shape[1], num_walk, num_dim)
+        
+        self.state_init = state_init
+        self.std_prop = std_prop
         self.num_step = num_step
         self.num_walk = num_walk
-        self.num_param = num_param
-        self.log_prob_func = log_prob_func
-        self.log_prob_args = log_prob_args
-        self.std_proposal = std_proposal
+        self.num_dim = num_dim
+        self.path_outfile = path_outfile
         self.frac_burn = frac_burn
-        self.initial = initial
-
-        self.rng = np.random.default_rng()
-        self.samples = np.zeros((self.num_step, self.num_walk, self.num_param))
-        self.samples[0] = self.initial
-        self.step_current = 0
+        self.seed = seed
+        self.log_prob_args = args
+        self.log_prob_kwargs = kwargs
+        
+        self.idx_step = 0
         self.rate_accept = 0.0
+        self.state_curr = self.state_init
+        self.rng = np.random.default_rng(self.seed)
+        self.num_step_burn = int(self.frac_burn * self.num_step)
+        self.samples = np.zeros((self.num_step - self.num_step_burn, self.num_walk, self.num_dim))
 
+    @abc.abstractmethod
+    def log_prob(self, x, **kwargs):
+        return 0.0
+    
     def step(self):
-        """
-        Step the Metropolis-Hastings algorithm.
-        """
-        current = self.samples[self.step_current]
-        proposal = current + self.rng.normal(
-            0, self.std_proposal, size=(self.num_walk, self.num_param)
+        """ Step the Metropolis-Hastings algorithm. """
+        # calculated the proposal state
+        state_prop = self.state_curr + self.rng.normal(
+            0, self.std_prop, size=(self.num_walk, self.num_dim)
         )
 
-        log_prob_current = self.log_prob_func(current, *self.log_prob_args)
-        log_prob_proposal = self.log_prob_func(proposal, *self.log_prob_args)
-        prob_accept = np.exp(log_prob_proposal - log_prob_current)
-
+        # calculate the acceptance probability
+        log_prob_curr = self.log_prob(self.state_curr, *self.log_prob_args, **self.log_prob_kwargs)
+        log_prob_prop = self.log_prob(state_prop, *self.log_prob_args, **self.log_prob_kwargs)
+        prob_accept = np.exp(log_prob_prop - log_prob_curr)
+        
+        # move accepted proposals into the current state
         cond_accept = self.rng.random(self.num_walk) < prob_accept
-        if self.step_current > self.frac_burn * self.num_step:
+        self.state_curr[cond_accept] = state_prop[cond_accept]
+        
+        # if burn-in stage is over, add current state to samples
+        if self.idx_step >= self.num_step_burn:
             self.rate_accept += np.sum(cond_accept)
+            self.samples[self.idx_step - self.num_step_burn] = self.state_curr
 
-        self.samples[self.step_current + 1] = current
-        self.samples[self.step_current + 1, cond_accept] = proposal[cond_accept]
-
-        self.step_current += 1
+        # increment the step index
+        self.idx_step += 1
 
     def run(self):
         """
@@ -86,5 +106,5 @@ class Hammer:
         for _i in range(self.num_step - 1):
             self.step()
 
-        self.samples = self.samples[int(self.frac_burn * self.num_step) :]
-        self.rate_accept /= self.num_step * self.num_walk * (1 - self.frac_burn)
+        # calculate the acceptance rate
+        self.rate_accept /= (self.num_step - self.num_step_burn) * self.num_walk
