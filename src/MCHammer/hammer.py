c64e9f742c567fef0c31d34312f84a581a5d73b9
zandalman <zandalman@gmail.com>
2024-10-14 08:53:04 -0500

    test: updated unit test for new hammer object

--- src/MCHammer/hammer.py
+++ src/MCHammer/hammer.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
-import numpy as np
+
 import abc
+import warnings
+
+import numpy as np
 
 __all__ = ["Hammer"]
 
@@ -15,7 +18,7 @@ class Hammer(abc.ABC):
 
     Args
         state_init (np.ndarray): The initial state.
-        std_prop (float):        The standard deviation of the proposal distirbution.
+        std_prop (float):        The standard deviation of the proposal distribution.
         num_step (int):          The number of steps.
         num_walk (int):          The number of walkers.
         num_dim (int):           The number of dimensions.
@@ -32,27 +35,30 @@ class Hammer(abc.ABC):
         num_sample (int):                     The number of samples.
         samples (np.ndarray):                 The sample array.
     """
+
     def __init__(
-            self, 
-            state_init, 
-            std_prop, 
-            num_step, 
-            num_walk, 
-            num_dim, 
-            path_outfile=None, 
-            frac_burn=0.2, 
-            seed=None, 
-            *args,
-            **kwargs
-        ):
-        
-        assert (frac_burn >= 0) and (frac_burn <= 1), \
-            "Burn fraction %.3g outside the allowed range [0, 1]." % frac_burn
-        
-        assert state_init.shape == (num_walk, num_dim), \
-            "Initial state shape (%d, %d) does not match number of walkers and number of dimensions (%d, %d)" % \
-            (state_init.shape[0], state_init.shape[1], num_walk, num_dim)
-        
+        self,
+        state_init,
+        std_prop,
+        num_step,
+        num_walk,
+        num_dim,
+        path_outfile=None,
+        frac_burn=0.2,
+        seed=None,
+        *args,
+        **kwargs,
+    ):
+        assert (
+            np.abs(frac_burn - 0.5) >= 0.5
+        ), f"Burn fraction {frac_burn:.3g} outside the allowed range [0, 1]."
+
+        assert state_init.shape == (
+            num_walk,
+            num_dim,
+        ), f"Initial state shape ({state_init.shape[0]:%d}, {state_init.shape[1]:%d}) \
+                does not match number of walkers and number of dimensions ({num_walk:%d}, {num_dim:%d})"
+
         self.state_init = state_init
         self.std_prop = std_prop
         self.num_step = num_step
@@ -63,34 +69,40 @@ class Hammer(abc.ABC):
         self.seed = seed
         self.log_prob_args = args
         self.log_prob_kwargs = kwargs
-        
+
         self.idx_step = 0
         self.rate_accept = 0.0
         self.state_curr = self.state_init
         self.rng = np.random.default_rng(self.seed)
         self.num_step_burn = int(self.frac_burn * self.num_step)
-        self.samples = np.zeros((self.num_step - self.num_step_burn, self.num_walk, self.num_dim))
+        self.samples = np.zeros(
+            (self.num_step - self.num_step_burn, self.num_walk, self.num_dim)
+        )
 
     @abc.abstractmethod
     def log_prob(self, x, **kwargs):
         return 0.0
-    
+
     def step(self):
-        """ Step the Metropolis-Hastings algorithm. """
+        """Step the Metropolis-Hastings algorithm."""
         # calculated the proposal state
         state_prop = self.state_curr + self.rng.normal(
             0, self.std_prop, size=(self.num_walk, self.num_dim)
         )
 
         # calculate the acceptance probability
-        log_prob_curr = self.log_prob(self.state_curr, *self.log_prob_args, **self.log_prob_kwargs)
-        log_prob_prop = self.log_prob(state_prop, *self.log_prob_args, **self.log_prob_kwargs)
+        log_prob_curr = self.log_prob(
+            self.state_curr, *self.log_prob_args, **self.log_prob_kwargs
+        )
+        log_prob_prop = self.log_prob(
+            state_prop, *self.log_prob_args, **self.log_prob_kwargs
+        )
         prob_accept = np.exp(log_prob_prop - log_prob_curr)
-        
+
         # move accepted proposals into the current state
         cond_accept = self.rng.random(self.num_walk) < prob_accept
         self.state_curr[cond_accept] = state_prop[cond_accept]
-        
+
         # if burn-in stage is over, add current state to samples
         if self.idx_step >= self.num_step_burn:
             self.rate_accept += np.sum(cond_accept)
@@ -108,3 +120,15 @@ class Hammer(abc.ABC):
 
         # calculate the acceptance rate
         self.rate_accept /= (self.num_step - self.num_step_burn) * self.num_walk
+        if self.rate_accept <= 0.1:
+            warnings.warn(
+                "Acceptance rate {self.rate_accept:%.3g} is very low.",
+                UserWarning,
+                stacklevel=2,
+            )
+        elif self.rate_accept >= 0.9:
+            warnings.warn(
+                "Acceptance rate {self.rate_accept:%.3g} is very high.",
+                UserWarning,
+                stacklevel=2,
+            )
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,37 +1,27 @@
 from __future__ import annotations
 
+import hammer
 import numpy as np
 
-import MCHammer
 
-
-def test_sampler():
+def test_gauss_hammer():
     """
     Test that the sampler recovers a multi-variate Gaussian distribution with approximately the correct standard deviation.
     """
-    num_step = 2**10
+
+    class GaussHammer(hammer.Hammer):
+        def log_prob(self, x, mu, sig):
+            return -0.5 * np.sum(((x - mu) / sig) ** 2, axis=-1)
+
+    std_prob = 1.65
+    num_step = 2**12
     num_walk = 32
     num_dim = 3
 
-    def log_prob_func(x, mu, sig):
-        return -0.5 * np.sum(((x - mu) / sig) ** 2, axis=-1)
+    state_init = np.zeros((num_walk, num_dim))
+    state_init[:] = np.linspace(-1, 1, num_walk)[:, None]
 
-    initial = np.zeros((num_walk, num_dim))
-    initial[:] = np.linspace(-1, 1, num_walk)[:, None]
-
-    hammer = MCHammer.Hammer(
-        "placeholder.h5",
-        num_step,
-        num_walk,
-        num_dim,
-        log_prob_func,
-        (np.zeros(num_dim), np.ones(num_dim)),
-        1.65,
-        initial,
-        frac_burn=0.2,
-    )
-
-    hammer.run()
-    std = np.std(hammer.samples.reshape((-1, num_dim)))
-
-    assert np.all(std) < 2.0
+    gh = GaussHammer(state_init, std_prob, num_step, num_walk, num_dim, mu=0, sig=1)
+    gh.run()
+    std = np.std(gh.samples, axis=(0, 1))
+    assert np.all(std) < 1.2
