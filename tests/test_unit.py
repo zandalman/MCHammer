d4e14b9a116747e821ea5e3c9f0b0543600ed765
Yue Pan <yuepan0928@gmail.com>
2024-12-10 14:12:19 -0500

    add testing function for SamplerStretch

--- tests/test_unit.py
+++ tests/test_unit.py
@@ -27,7 +27,7 @@ def test_sampler_stretch():
     def log_prob_func(x, mu=0, sig=1):
         return -0.5 * np.sum(((x - mu) / sig) ** 2, axis=-1)
 
-    sampler = SamplerStretch(
+    sampler = mchammers.SamplerStretch(
         num_step=2**12,
         num_walker=32,
         num_dim=3,
@@ -42,3 +42,5 @@ def test_sampler_stretch():
     print(f"Standard deviation of samples: {std}")
     assert np.all(std < 1.2), f"Standard deviation out of bounds: {std}"
     return sampler.samples
+
+
